8a0c809805f6f5f5a41e2dd693dffcc8d3b92fc8
omishashree <omishashree@gmail.com>
2026-05-22 21:57:59 +0530

    frontend commit

--- expense-tracker/src/pages/auth/SignUp.jsx
+++ expense-tracker/src/pages/auth/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import Input from "../../components/Inputs/temp";
-import ProfilePhotoSelector from "../../components/inputs/ProfilePhotoSelector";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { useContext } from "react";

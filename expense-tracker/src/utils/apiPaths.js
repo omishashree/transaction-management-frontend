e2f2dfa37f63fbfdf38272366a37fa02936d3693
omishashree <omishashree@gmail.com>
2026-05-22 22:44:23 +0530

    Fixed cors completely

--- expense-tracker/src/utils/apiPaths.js
+++ expense-tracker/src/utils/apiPaths.js
@@ -1,28 +1,39 @@
-export const BASE_URL = "https://transaction-management-4pxm.onrender.com/api/v1";
+export const BASE_URL =
+  "https://transaction-management-4pxm.onrender.com/api/v1";
 
 // utils/apiPaths.js
 export const API_PATHS = {
+
   AUTH: {
-    LOGIN: "/api/v1/auth/login",
-    REGISTER: "/api/v1/auth/register",
-    GET_USER: "/api/v1/auth/getUser",
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    GET_USER: "/auth/getUser",
   },
+
   DASHBOARD: {
-    GET_DATA: "/api/v1/dashboard",
+    GET_DATA: "/dashboard",
   },
+
   INCOME: {
-    ADD_INCOME: "/api/v1/income/add",
-    GET_ALL_INCOME: "/api/v1/income/get",
-    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
-    DOWNLOAD_INCOME: "/api/v1/income/downloadexcel",
+    ADD_INCOME: "/income/add",
+    GET_ALL_INCOME: "/income/get",
+    DELETE_INCOME: (incomeId) =>
+      `/income/${incomeId}`,
+    DOWNLOAD_INCOME:
+      "/income/downloadexcel",
   },
+
   EXPENSE: {
-    ADD_EXPENSE: "/api/v1/expense/add",
-    GET_ALL_EXPENSE: "/api/v1/expense/get",
-    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE: "/api/v1/expense/downloadexcel",
+    ADD_EXPENSE: "/expense/add",
+    GET_ALL_EXPENSE: "/expense/get",
+    DELETE_EXPENSE: (expenseId) =>
+      `/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE:
+      "/expense/downloadexcel",
   },
+
   IMAGE: {
-    UPLOAD_IMAGE: "/api/v1/auth/upload-image",
+    UPLOAD_IMAGE: "/auth/upload-image",
   },
+
 };

f3bbd93f705b0b17c5783e8881aab091df9a9939
omishashree <omishashree@gmail.com>
2026-05-22 20:58:38 +0530

    Fixed import casing

--- expense-tracker/src/components/Income/AddIncomeForm.jsx
+++ expense-tracker/src/components/Income/AddIncomeForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Input from "../Inputs/input";
+import Input from "../Inputs/Input";
 import EmojiPickerPopup from "../EmojiPickerPopup";
 const AddIncomeForm = ({ onAddIncome }) => {
 
